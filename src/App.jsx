19beaf25f3b110c772749e2f077862f9880d5d51
remant <paudelremant45@gmail.com>
2024-03-09 08:43:45 +0545

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import NavBar from './components/NavBar'
 import CarouselSection from './components/CarouselSection'
 import AboutUs from './components/AboutUs'
 import Testimonials from './components/Testimonials'
