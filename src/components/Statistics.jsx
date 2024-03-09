27aa9f1f7e268ba08393dd27204180e00154de55
remant <paudelremant45@gmail.com>
2024-03-09 08:42:43 +0545

    updated componenets

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -6,35 +6,48 @@ const Statistics = () => {
   const [counterStart, setCounterStart] = useState(false);
 
   return (
-    <div className="py-24">
-      <div className="container mx-auto">
-        <h2 className="text-white text-center text-4xl font-bold mb-4 pb-16">
-          Accomplishments
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <div className="bg-[#7b7b7e] backdrop-filter backdrop-blur-lg bg-opacity-15 px-4 py-20 rounded-lg text-center">
-            <h3 className="text-white text-3xl font-normal">No of Clients</h3>
-            <p className="text-white text-3xl pt-4 font-bold">
-              <CountUp start={0} end={1000} duration={6} delay={0} />+
-            </p>
-          </div>
-          <div className="bg-[#7b7b7e] backdrop-filter backdrop-blur-lg bg-opacity-15 px-4 py-20 rounded-lg text-center">
-            <h3 className="text-white text-3xl font-normal">
-              No of Projects Completed
-            </h3>
-            <p className="text-white text-3xl pt-4 font-bold">
-              <CountUp start={0} end={500} duration={3} delay={0} />+
-            </p>
-          </div>
-          <div className="bg-[#7b7b7e] backdrop-filter backdrop-blur-lg bg-opacity-15 px-4 py-20 rounded-lg text-center">
-            <h3 className="text-white text-3xl font-normal">No of Products</h3>
-            <p className="text-white text-3xl pt-4 font-bold">
-              <CountUp start={0} end={50} duration={3} delay={0} />+
-            </p>
+    <ScrollTrigger onEnter={() => setCounterStart(true)} onExit={() => setCounterStart(false)}>
+      <div className="py-24">
+        <div className="container mx-auto">
+          <h2 className="text-white text-center text-4xl font-bold mb-4 pb-16">
+            Accomplishments
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div className="bg-[#7b7b7e] backdrop-filter backdrop-blur-lg bg-opacity-15 px-4 py-20 rounded-lg text-center">
+              <h3 className="text-white text-3xl font-normal">No of Clients</h3>
+              <p className="text-white text-3xl pt-4 font-bold">
+                {counterStart && (
+                  <CountUp start={0} end={1000} duration={6} delay={0} />
+                )}
+                +
+              </p>
+            </div>
+            <div className="bg-[#7b7b7e] backdrop-filter backdrop-blur-lg bg-opacity-15 px-4 py-20 rounded-lg text-center">
+              <h3 className="text-white text-3xl font-normal">
+                No of Projects Completed
+              </h3>
+              <p className="text-white text-3xl pt-4 font-bold">
+                {counterStart && (
+                  <CountUp start={0} end={500} duration={3} delay={0} />
+                )}
+                +
+              </p>
+            </div>
+            <div className="bg-[#7b7b7e] backdrop-filter backdrop-blur-lg bg-opacity-15 px-4 py-20 rounded-lg text-center">
+              <h3 className="text-white text-3xl font-normal">
+                No of Products
+              </h3>
+              <p className="text-white text-3xl pt-4 font-bold">
+                {counterStart && (
+                  <CountUp start={0} end={50} duration={3} delay={0} />
+                )}
+                +
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </ScrollTrigger>
   );
 };
 
